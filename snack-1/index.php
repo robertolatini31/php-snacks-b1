0a031e4644c961bc0de182442b487cb3d3028dcd
robertolatini31 <robertolatini31@gmail.com>
2022-05-20 12:39:45 +0200

    initial commit

---
+++ snack-1/index.php
@@ -0,0 +1,53 @@
+<!-- ## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra
+di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php
+
+$match_day = [
+   [
+       home =>'roma',
+       guest =>'milano',
+       points_home =>'63',
+       points_guest =>'52',
+   ], 
+   [
+        home =>'napoli',
+        guest =>'trieste',
+        points_home =>'88',
+        points_guest =>'62',
+    ],
+    [
+        home =>'bari',
+        guest =>'genova',
+        points_home =>'43',
+        points_guest =>'72',
+    ],
+    [
+        home =>'perugia',
+        guest =>'terni',
+        points_home =>'23',
+        points_guest =>'92',
+    ], 
+];
+
+var_dump($match_day);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
