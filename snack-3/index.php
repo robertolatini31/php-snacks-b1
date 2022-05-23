bad86e3f24fa8abc9aa7312fa66670c35c6773ca
robertolatini31 <robertolatini31@gmail.com>
2022-05-23 11:53:56 +0200

    add snack-3

---
+++ snack-3/index.php
@@ -0,0 +1,79 @@
+<!-- ## Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave 
+una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un 
+array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+
+<?php
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snack-3</title>
+</head>
+<body>
+    
+    <?php foreach ($posts as $key_date => $array_posts) : ?>
+        <h3>
+            <?php echo $key_date ?>
+        </h3>
+        <ul>
+            <?php foreach ($array_posts as $array_post) : ?>
+                <?php foreach ($array_post as $key => $value) : ?>
+                    <li>
+                        <?php echo $key . ': ' . $value ?>
+                    </li>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+        </ul>
+    <?php endforeach; ?>
+
+
+</body>
+</html>
